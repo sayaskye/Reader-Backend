7d904c0c1998f957c60e3c3868954704493cd2bc
Andres Cazares <chaotears@gmail.com>
2026-02-07 23:30:42 -0700

    jose jwt utils

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,11 +1,23 @@
-import { SignJWT } from "jose";
+import { jwtVerify, SignJWT } from "jose";
 
-export const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
 export async function createJWT(userId: string) {
   return new SignJWT({ sub: userId })
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime("15m")
+    .setExpirationTime("5s")
     .sign(secret);
+}
+
+export async function createRefreshToken(userId: string) {
+  return new SignJWT({ sub: userId, type: "refresh" })
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(secret);
+}
+
+export async function verify(token: string) {
+  return jwtVerify(token, secret);
 }
