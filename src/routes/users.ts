0427a771ddb8baf28e540a4f56524c47512d1bfd
Andres Cazares <chaotears@gmail.com>
2026-02-10 20:17:22 -0700

    fix(users): disable not needed endpoints from users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,19 +1,16 @@
 import { Hono } from "hono";
 
 import { UsersController } from "@/controllers/users";
-import {
-  validatePartialUser,
-  validateUser,
-  validateUUID,
-} from "@/schemas/users";
+import { validatePartialUser, validateUUID } from "@/schemas/users";
 
 import { validateBody, validateParam } from "@/middlewares/zod-validators";
 
 export const users = new Hono();
-
+//TODO: Adjust endpoints by roles, only admin can delete and watch all users
 users.get("/", UsersController.getAll);
 users.get("/:id", validateParam(validateUUID, "id"), UsersController.getId);
-users.post("/", validateBody(validateUser), UsersController.create);
-users.put("/:id", validateBody(validatePartialUser), validateParam(validateUUID, "id"), UsersController.update);
 users.patch("/:id", validateBody(validatePartialUser), validateParam(validateUUID, "id"), UsersController.partialUpdate);
 users.delete("/:id", validateParam(validateUUID, "id"), UsersController.delete);
+//Disabled, there is no need to make this endpoints in reality
+//users.post("/", validateBody(validateUser), UsersController.create);
+//users.put("/:id", validateBody(validatePartialUser), validateParam(validateUUID, "id"), UsersController.update);
